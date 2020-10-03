94717fb822c2300997b9a6f254bc8ac2289e2007
tsubasan1 <69135934+tsubasan1@users.noreply.github.com>
2020-10-03 23:27:42 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -46,26 +46,3 @@ $(function () {
     return false;
   });
 });
-
-$('.box').waypoint({
-  handler(direction) {
-      // 要素が画面中央に来るとここが実行される
-      if (direction === 'down') {
-        /**
-         * 下方向のスクロール
-         * イベント発生元の要素にfadeInUpクラスを付けることで
-         * アニメーションを開始
-         */
-        $(this.element).addClass('fadeInUp');
-  
-        /**
-         * waypointを削除することで、この要素に対しては
-         * これ以降handlerが呼ばれなくなる
-         */
-        this.destroy();
-      }
-    },
-  
-    // 要素が画面中央に来たらhandlerを実行
-    offset: '50%',
-  });
